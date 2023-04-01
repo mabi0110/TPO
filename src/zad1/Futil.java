3855be87b3b6ee48f851e362ed90e45612c68d97
MaBi0110 <biziewskamk@gmail.com>
2023-04-01 09:34:20 +0200

    add zad 1

--- src/zad1/Futil.java
+++ src/zad1/Futil.java
@@ -1,6 +1,48 @@
 package zad1;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.nio.file.StandardOpenOption.*;
+
 public class Futil {
     public static void processDir(String dirName, String resultFileName) {
+        Path startPath = Paths.get(dirName);
+        Path resultPath = Paths.get(resultFileName);
+
+        Charset csIn = Charset.forName("Cp1250");
+        Charset csOut = Charset.forName("UTF-8");
+
+        List<Path> plist = new ArrayList<>();
+        try (Stream<Path> paths = Files.walk(startPath)) {
+            //paths.forEach(p -> System.out.println(p));
+            plist = paths.filter(Files::isRegularFile).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (FileChannel fcOut = FileChannel.open(resultPath, CREATE, TRUNCATE_EXISTING, WRITE)) {
+            for (Path p: plist) {
+                try (FileChannel fcIn = FileChannel.open(p)) {
+                    ByteBuffer bb = ByteBuffer.allocateDirect((int) fcIn.size());
+                    fcIn.read(bb);
+                    bb.flip();
+                    CharBuffer cb = csIn.decode(bb);
+                    fcOut.write(csOut.encode(cb));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -1,16 +1,15 @@
 /**
- *
- *  @author Biziewska Małgorzata S22275
- *
+ * @author Biziewska Małgorzata S22275
  */
 
 package zad1;
 
-
 public class Main {
-  public static void main(String[] args) {
-    String dirName = System.getProperty("user.home")+"/TPO1dir";
-    String resultFileName = "TPO1res.txt";
-    Futil.processDir(dirName, resultFileName);
-  }
+
+    public static void main(String[] args) {
+        String dirName = System.getProperty("user.home") + "/TPO1dir";
+        String resultFileName = "TPO1res.txt";
+        Futil.processDir(dirName, resultFileName);
+    }
 }
+
